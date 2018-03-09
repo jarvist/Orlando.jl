253148a54256cb127bc95b3248f772ebcfdbe84e
Jarvist Moore Frost <jarvist@gmail.com>
2018-03-09 02:56:11 +0000

    Gotta start on a quote.

--- src/Orlando.jl
+++ src/Orlando.jl
@@ -1,4 +1,7 @@
+# Orlando.jl - 
 module Orlando
+
+println("    \"Life, it sings, or croons rather, like a kettle on a hob, Life, life what art thou?\" - Virginia Woolf, Orlando: a Biography")
 
 # package code goes here
 
