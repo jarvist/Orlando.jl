3f394ca22a998c38dfbec99321aa342da275c7c4
Jarvist Moore Frost <jarvist@gmail.com>
2019-03-26 21:45:15 +0000

    Dragged kicking and screaming into Julia 1.x; with modern Pkg.

--- src/Orlando.jl
+++ src/Orlando.jl
@@ -1,6 +1,11 @@
 # Orlando.jl - 
 module Orlando
 
+using Flux
+#using TightBinding # currently a bit too heavy + doesn't play well
+using Nabla
+using LinearAlgebra
+
 println("    \"Life, it sings, or croons rather, like a kettle on a hob, Life, life what art thou?\" - Virginia Woolf, Orlando: a Biography")
 
 # package code goes here
